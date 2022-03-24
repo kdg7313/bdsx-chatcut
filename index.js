5c274b98a3f2d2c50ff696c24febbebb46a37fdf
kdg7313 <76900744+kdg7313@users.noreply.github.com>
2022-03-24 23:43:24 +0900

    Update

--- index.js
+++ index.js
@@ -1,35 +1,29 @@
+"use strict";
 /*
 Made By corcle1 (omlet : kdg7313) (github : kdg7313)
 */
-
-import { MinecraftPacketIds } from "../bdsx/bds/packetids";
-import { CANCEL } from "../bdsx/common";
-import { events } from "../bdsx/event";
-import * as color from "colors";
-
+Object.defineProperty(exports, "__esModule", { value: true });
+const packetids_1 = require("bdsx/bds/packetids");
+const common_1 = require("bdsx/common");
+const event_1 = require("bdsx/event");
+const launcher_1 = require("bdsx/launcher");
+const color = require("colors");
 console.log(color.bgRed('Chatcut Applying...'));
-
-let time = {}
-
+let time = {};
 const system = server.registerSystem(0, 0);
-
-events.packetBefore(MinecraftPacketIds.Text).on(ev => {
-   if(time[ev.name] === undefined){
-      time[ev.name] = new Date();
-   }
-   else if(Number(new Date()) - time[ev.name] < 1000){
-      system.executeCommand(`tellraw @a[name="${ev.name}"] {"rawtext":[{"text":"§l§cYour chat is too fast!"}]}`, () => {}); //send a warning message to the player
-      return CANCEL;
-   }
-   else {
-  time[ev.name] = new Date();
-      }
-
-   if (ev.message.length > 25) 
-   {
-	   system.executeCommand(`tellraw @a[name="${ev.name}"] {"rawtext":[{"text":"§l§cYour chat is too long!"}]}`, () => {}); //send a warning message to the player
-      return CANCEL; 
-   }
+event_1.events.packetBefore(packetids_1.MinecraftPacketIds.Text).on(ev => {
+    if (time[ev.name] === undefined)
+        time[ev.name] = new Date();
+    else if (Number(new Date()) - time[ev.name] < 1000) {
+        launcher_1.bedrockServer.executeCommand(`tellraw @a[name="${ev.name}"] {"rawtext":[{"text":"§l§cYour chat is too fast!"}]}`); //send a warning message to the player
+        return common_1.CANCEL;
+    }
+    else {
+        time[ev.name] = new Date();
+    }
+    if (ev.message.length > 25) {
+        launcher_1.bedrockServer.executeCommand(`tellraw @a[name="${ev.name}"] {"rawtext":[{"text":"§l§cYour chat is too long!"}]}`); //send a warning message to the player
+        return common_1.CANCEL;
+    }
 });
-
 console.log(color.bgGreen('Application complete!'));
